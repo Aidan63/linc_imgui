e4deb817f8d270b729617bc8235d02af89dd6357
Aidan Lee <aidan.lee63@gmail.com>
2017-12-10 00:04:08 +0000

    Commented and added two missing members to ImDrawCmd

--- imgui/draw/ImDrawCmd.hx
+++ imgui/draw/ImDrawCmd.hx
@@ -1,8 +1,11 @@
 package imgui.draw;
 
-import cpp.Pointer;
-import cpp.Void;
+import imgui.ImGui;
 import imgui.util.ImVec4;
+import cpp.Pointer;
+import cpp.ConstPointer;
+import cpp.Void;
+import cpp.Callable;
 
 @:keep
 @:include('linc_imgui.h')
@@ -11,7 +14,32 @@ import imgui.util.ImVec4;
 @:unreflective
 extern class ImDrawCmd
 {
-    public var ElemCount : Int;
+    /**
+      Number of indices (multiple of 3) to be rendered as triangles.
+      
+      Vertices are stored in the callee ImDrawList's vtx_buffer[] array, indices in idx_buffer[].
+     */
+    public var ElemCount : UInt;
+
+    /**
+      Clipping rectangle (x1, y1, x2, y2)
+     */
     public var ClipRect : ImVec4;
-    public var TextureId : Pointer<Void>;
+
+    /**
+      User-provided texture ID. Set by user in ImfontAtlas::SetTexID() for fonts or passed to Image*() functions.
+      
+      Ignore if never using images or multiple fonts atlas.
+     */
+    public var TextureId : ImTextureID;
+
+    /**
+      If != NULL, call the function instead of rendering the vertices. clip_rect and texture_id will be set normally.
+     */
+    public var UserCallback : Callable<ConstPointer<ImDrawList>->ConstPointer<ImDrawCmd>->Void>;
+
+    /**
+      The draw callback code can access this.
+     */
+    public var UserCallbackData : Pointer<Void>;
 }
