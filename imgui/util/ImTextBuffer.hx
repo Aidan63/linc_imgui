2fa51bd522879e9c559686971a7d01579848587c
Aidan Lee <aidan.lee63@gmail.com>
2017-12-19 21:05:59 +0000

    Added simple text storage class to avoid dealing with Array<Char>'s

---
+++ imgui/util/ImTextBuffer.hx
@@ -0,0 +1,31 @@
+package imgui.util;
+
+import cpp.Char;
+import cpp.Pointer;
+import StringBuf;
+
+class ImTextBuffer
+{
+    public var buffer(default, null) : Array<Char>;
+
+    public function new(_length : Int, ?_initialString : String)
+    {        trace(txtBuf.toString());
+        buffer = [ for (i in 0..._length) 0 ];
+        if (_initialString != null) setString(_initialString);
+    }
+
+    public function setString(_text : String)
+    {
+        for (i in 0..._text.length)
+        {
+            buffer[i] = _text.charCodeAt(i);
+        }
+    }
+
+    public function toString() : String
+    {
+        var buf = new StringBuf();
+        for (char in buffer) buf.addChar(char);
+        return buf.toString();
+    }
+}
