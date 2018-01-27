83818c7625bd66eee48edb16979f70991062491b
Aidan Lee <aidan.lee63@gmail.com>
2018-01-27 22:11:02 +0000

    Added proper ImGuiTextBuffer using ImVector and removed temp replacement

---
+++ imgui/util/ImGuiTextBuffer.hx
@@ -0,0 +1,29 @@
+package imgui.util;
+
+import imgui.ImGui;
+
+@:include('linc_imgui.h')
+@:native('ImGuiTextBuffer')
+@:structAccess
+@:unreflective
+extern class ImGuiTextBuffer
+{
+    @:native('Buf') public var buf : ImVectorImWchar;
+
+    @:native('ImGuiTextBuffer') public static function create() : ImGuiTextBuffer;
+
+    public inline function begin() : String {
+        return cast untyped __cpp__('{0}.begin()', this);
+    }
+    public inline function end() : String {
+        return cast untyped __cpp__('{0}.end()', this);
+    }
+    @:native('size')    public function size() : Int;
+    @:native('empty')   public function empty() : Bool;
+    @:native('clear')   public function clear() : Void;
+    @:native('reserve') public function reserve() : Void;
+    @:native('c_str')   public function c_str() : cpp.ConstCharStar;
+    public inline function append(_string : String) : Void {
+        untyped __cpp__('{0}.appendf("%s", {1}.c_str())', this, _string);
+    }
+}
