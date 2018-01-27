28a0736a1ddbfaad9b4184cd1fcdd519c79ee6a1
Aidan Lee <aidan.lee63@gmail.com>
2018-01-27 16:43:15 +0000

    Updated ImFontConfig struct haxe types

--- imgui/font/ImFontConfig.hx
+++ imgui/font/ImFontConfig.hx
@@ -1,7 +1,6 @@
 package imgui.font;
 
 import cpp.Pointer;
-import cpp.ConstPointer;
 import imgui.ImGui;
 import imgui.util.ImVec2;
 
@@ -12,18 +11,19 @@ import imgui.util.ImVec2;
 @:unreflective
 extern class ImFontConfig
 {
-    @:native('FontData')            public var fontData : Pointer<Void>;
+    @:native('FontData')            public var fontData : cpp.RawPointer<cpp.Void>;
     @:native('FontDataSize')        public var fontDataSize : Int;
     @:native('FontDataOwnedByAtas') public var fontDataOwnedByAtas : Bool;
     @:native('FontNo')              public var fontNo : Int;
     @:native('SizePixels')          public var sizePixels : Float;
     @:native('OversampleH')         public var oversampleH : Int;
     @:native('OversampleV')         public var oversampleV : Int;
+    @:native('PixelSnapH')          public var pixelSnapH : Bool;
     @:native('GlyphExtraSpacing')   public var glyphExtraSpacing : ImVec2;
     @:native('GlyphOffset')         public var glyphOffset : ImVec2;
-    @:native('GlyphRanges')         public var glyphRanges : ConstPointer<ImWchar>;
+    @:native('GlyphRanges')         public var glyphRanges : cpp.RawConstPointer<ImWchar>;
     @:native('MergeMode')           public var mergeMode : Bool;
-    @:native('RasterizerFlags')     public var rasterizerFlags : UInt;
+    @:native('RasterizerFlags')     public var rasterizerFlags : Int;
     @:native('RasterizerMultiply')  public var rasterizerMultiply : Float;
 
     @:native('new ImFontConfig') static function create() : Pointer<ImFontConfig>;
