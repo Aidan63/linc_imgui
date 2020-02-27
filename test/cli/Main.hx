2745eefcb4643d901a6f22d89a984e0f9f9fd24e
Aidan Lee <aidan.lee63@gmail.com>
2020-02-27 12:54:53 +0000

    Update cli test and remove haxeflixel and luxe sample

--- test/cli/Main.hx
+++ test/cli/Main.hx
@@ -1,4 +1,5 @@
 
+import cpp.Pointer;
 import imgui.ImGui;
 
 using cpp.Native;
@@ -16,26 +17,36 @@ class Main
         final io  = ImGui.getIO();
         io.displaySize = ImVec2.create(640, 480);
 
-        final id = 'text';
+        final id = "Some ID";
 
-        var width  = 0;
-        var height = 0;
+        var width  = [ 0 ];
+        var height = [ 0 ];
         var pixels : cpp.Star<cpp.UInt8> = null;
         var pixelPtr : cpp.Star<cpp.Star<cpp.UInt8>> = pixels.addressOf();
         
         io.fonts.getTexDataAsRGBA32(pixelPtr, width, height);
-        io.fonts.texID = id;
+        io.fonts.texID = Pointer.addressOf(id).raw;
         io.backendRendererName = 'my renderer';
+
+        trace(width[0], height[0]);
 
         // Update
         ImGui.newFrame();
         
         ImGui.showDemoWindow();
 
+        if (ImGui.begin('window'))
+        {
+            final ints = [ 1, 2, 3, 4 ];
+
+            ImGui.sliderInt4('multi ints', ints, 0, 10);
+        }
+        ImGui.end();
+
         ImGui.render();
 
         ImGui.destroyContext(ctx);
-        
+
         trace('done');
     }
 }
